5557e638f5ae7616f5886ff68627b30637006711
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-10-19 23:29:59 +0530

    Time: 7 ms (89.01%), Space: 11 MB (81.12%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -6,7 +6,13 @@ public:
         for(auto ch:s){
             mpp[ch]++;
         }
-        priority_queue<pair<int,char>>pq;
+         auto comp = []( pair<int, char>& a,  pair<int, char>& b) {
+            if (a.first == b.first) {
+                return a.second > b.second; 
+            }
+            return a.first < b.first; 
+        };
+        priority_queue<pair<int,char>,vector<pair<int, char>>,decltype(comp)>pq;
         for(auto it:mpp){
             pq.push({it.second,it.first});
         }
