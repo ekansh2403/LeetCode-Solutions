0b954c1d00757b7bc915c0a4a337c81967aae80f
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-10-21 11:47:33 +0530

    Time: 0 ms (100%), Space: 12.5 MB (69.8%) - LeetHub

--- 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
-        int n = nums.size();
-        int cnt = 0;  // To count the number of violations
-        
-        for (int i = 1; i < n; i++) {
-            if (nums[i] <= nums[i - 1]) {
+        int n=nums.size();
+        int cnt=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<=nums[i-1]){
                 cnt++;
-                if (cnt > 1) return false;  // More than one violation
+                if(cnt>1) return false;
+
                 
-                // Check if removing nums[i-1] or nums[i] will fix the issue
-                if (i > 1 && nums[i] <= nums[i - 2] && i < n - 1 && nums[i + 1] <= nums[i - 1]) {
-                    return false;  // Neither removal works, so return false
-                }
+                    if(i>1&&nums[i]<=nums[i-2]&&i<n-1&& nums[i+1]<=nums[i-1]){
+                        return false;
+                    }
+                
             }
         }
-        
         return true;
     }
 };
