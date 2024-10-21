c97bb541a809e9c24c60bfbb1446d8d2154eaae5
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-10-21 11:40:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (69.8%) - LeetHub

--- 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-bool isSorted(vector<int>&nums){
-    for(int i=1;i<nums.size();i++){
-        if(nums[i]<=nums[i-1]) return false;
-    }
-    return true;
-}
     bool canBeIncreasing(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-                vector<int>temp;
-            for(int j=0;j<n;j++){
-                if(i!=j) temp.push_back(nums[j]);
+        int n = nums.size();
+        int cnt = 0;  // To count the number of violations
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                cnt++;
+                if (cnt > 1) return false;  // More than one violation
+                
+                // Check if removing nums[i-1] or nums[i] will fix the issue
+                if (i > 1 && nums[i] <= nums[i - 2] && i < n - 1 && nums[i + 1] <= nums[i - 1]) {
+                    return false;  // Neither removal works, so return false
+                }
             }
-            if(isSorted(temp)) return true;
         }
-        return false;
+        
+        return true;
     }
 };
