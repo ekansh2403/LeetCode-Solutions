9db21a8e0359ab8af1dc98f3c5161d3a7fe2dd81
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-10-28 12:30:02 +0530

    Time: 92 ms (60%), Space: 108.7 MB (80.39%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        sort(nums.begin(),nums.end());
-        int ans=0;
-        for(auto it :nums){
-            int sqroot=(int)sqrt(it);
-            if(sqroot*sqroot==it&& mpp.find(sqroot)!=mpp.end()){
-                mpp[it]=mpp[sqroot]+1;
+        // unordered_map<int,int>mpp;
+        // sort(nums.begin(),nums.end());
+        // int ans=0;
+        // for(auto it :nums){
+        //     int sqroot=(int)sqrt(it);
+        //     if(sqroot*sqroot==it&& mpp.find(sqroot)!=mpp.end()){
+        //         mpp[it]=mpp[sqroot]+1;
+
+        //     }
+        //     else {
+        //         mpp[it]=1;
+        //     }
+        //     ans=max(ans,mpp[it]);
+        // }
+        // return (ans<2?-1:ans);
+
+        //Approach 2
+        unordered_set<int>st(nums.begin(),nums.end());
+         int ans=0;
+         for(auto num:nums){
+            long long curr=num;
+            int streak=0;
+            while(st.find(curr)!=st.end()){
+                streak++;
+                if(curr*curr>1e5) break;
+                curr*=curr;
 
             }
-            else {
-                mpp[it]=1;
-            }
-            ans=max(ans,mpp[it]);
-        }
-        return (ans<2?-1:ans);
+            ans=max(ans,streak);
+         }
+         return ans<2?-1:ans;
     }
 };
