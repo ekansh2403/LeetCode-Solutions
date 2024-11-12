9ea3c7d306ea0a5603550259b825cc336d05530c
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-11-12 12:02:25 +0530

    Time: 52 ms (71.31%), Space: 92.2 MB (85.47%) - LeetHub

---
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& q) {
+        // i have custom sorted the items  
+        sort(items.begin(),items.end(),[](vector<int>&a,vector<int>&b){
+            if(a[0]==b[0]){
+                return a[1]>b[1];
+            }
+            return a[0]<b[0];
+        });
+
+        //change the beauty with maximum beauty
+        int mxb=items[0][1];
+        int n=items.size();
+        for(int i=0;i<n;i++){
+           mxb=max(mxb,items[i][1]);
+           items[i][1]=mxb;
+        }
+
+//   for (const auto& it : items) {
+//         for (int i : it) {
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+
+        vector<int>res;
+        int m=q.size();
+        for(int i=0;i<m;i++){
+            int l=0;
+            int r=n-1;
+            int ans=0;
+            while(l<=r){
+                int mid=r+(l-r)/2;
+                if(items[mid][0]<=q[i]){
+                    ans=max(ans,items[mid][1]);
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+            res.push_back(ans);
+        }
+        return res;
+        
+    
+        
+    }
+};
