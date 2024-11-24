90209140c5f568d21a2f622d8b76fbb6677a96ea
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-11-24 14:07:18 +0530

    Time: 4 ms (31.34%), Space: 38.2 MB (87.5%) - LeetHub

---
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+using ll =long long;
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        //1. find the absolute sum of the whole matrix
+        //2. then check for the cnt of negative number 
+                //if it is odd then sum-2*smallest neg number
+                // if it is even then sum is the ans
+
+        ll cnt=0;
+        ll sum=0;
+        int n=matrix.size();
+        int small=INT_MAX;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                sum+=abs(matrix[i][j]);
+                if(matrix[i][j]<0){
+                    cnt++;
+                }
+                small= min(small,abs(matrix[i][j]));
+
+            }
+        }
+        if(cnt%2==0) return sum;
+        return sum-2*small;
+
+        
+    }
+};
