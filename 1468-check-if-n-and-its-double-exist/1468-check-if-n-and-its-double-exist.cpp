1ff33f278458203700026cadda372f230ecd6619
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-12-01 10:15:48 +0530

    Time: 0 ms (100%), Space: 13.5 MB (24.36%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
       int n=arr.size();
-      unordered_map<int,int>mpp;
-      for(auto it:arr){
-        mpp[it]++;
-      }
-      for(auto it:arr){
-        if(it==0){
-            if(mpp[it]>1) return 1;
-        }
-        else if(mpp.count(it*2)){
-            return 1;
+    //   unordered_map<int,int>mpp;
+    //   for(auto it:arr){
+    //     mpp[it]++;
+    //   }
+    //   for(auto it:arr){
+    //     if(it==0){
+    //         if(mpp[it]>1) return 1;
+    //     }
+    //     else if(mpp.count(it*2)){
+    //         return 1;
+    //     }
+    //   }
+    //   return 0;
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i!=j&&arr[i]==2*arr[j]){
+                return 1;
+            }
         }
       }
       return 0;
