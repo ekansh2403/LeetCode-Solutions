52d0312fe994bee907a6f6a028362a7cc0887370
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-12-02 10:20:57 +0530

    Time: 0 ms (100%), Space: 8.3 MB (6.42%) - LeetHub

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPrefix(string str,string word){
+        return str.substr(0,word.size())==word;
+    }
+    int isPrefixOfWord(string s, string w) {
+        stringstream ss(s);
+        string token;
+        vector<string>word;
+        while(ss>>token){
+            word.push_back(token);
+        }
+        int n=word.size();
+        int m=w.length();
+        int ans=-1;
+        bool flag;
+        for(int i=0;i<n;i++){
+           if(isPrefix(word[i],w)){
+                ans=i+1;
+                break;
+           }
+        }
+        return ans;
+    }
+};
