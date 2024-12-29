543b7afe76f4792152dac06ab6315205fa131821
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2024-12-29 17:27:27 +0530

    Time: 0 ms (100%), Space: 28.2 MB (8.33%) - LeetHub

---
+++ 3691-minimum-operations-to-make-columns-strictly-increasing/3691-minimum-operations-to-make-columns-strictly-increasing.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumOperations(vector<vector<int>>& grid) {
+        int ans=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        for(int j=0;j<m;j++){
+            for(int i=1;i<n;i++){
+                if(grid[i][j] < grid[i-1][j]+1){
+                    ans+=grid[i-1][j]-grid[i][j]+1;
+                    grid[i][j]=grid[i-1][j]+1;
+                }
+            }
+        }
+        return ans;
+    }
+};
