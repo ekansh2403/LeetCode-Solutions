99568c81920796ca1cf1f92e67560eb74f8c6fef
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2025-01-13 10:56:47 +0530

    Time: 51 ms (40.73%), Space: 32.6 MB (36.69%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -2,16 +2,26 @@ class Solution {
 public:
     int minimumLength(string s) {
         unordered_map<char, int> mpp;
-        int del=0;
-        for (auto& ch : s) {
-            mpp[ch]+=1;
-            if(mpp[ch]==3){
-                mpp[ch]-=2;
-                del+=2;
-            }
+        // int del=0;
+        // for (auto& ch : s) {
+        //     mpp[ch]+=1;
+        //     if(mpp[ch]==3){
+        //         mpp[ch]-=2;
+        //         del+=2;
+        //     }
+        // }
+        // return s.size()-del;
+        
+
+        for(auto &ch:s){
+            mpp[ch]++;
         }
-        return s.size()-del;
-        
-        
+        int ans=0;
+        for(auto &it:mpp){
+            if(it.second==0) continue;
+            else if(it.second%2==0) ans+=2;
+            else ans++;
+        }
+        return ans;
     }
 };
